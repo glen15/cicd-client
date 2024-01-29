15f685cbd5708c088c6a186fe9a6f6fcc4ba1783
glen15 <glen15@naver.com>
2024-01-29 12:37:54 +0900

    change code

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+// 'REACT_APP_' prefix 필수
+const { REACT_APP_API_URL } = process.env;
+console.log({ REACT_APP_API_URL });
 function App() {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
 
   useEffect(() => {
-    // 페이지 로드 시 데이터를 불러옵니다.
     fetchNotes();
-
-    // 10초마다 페이지 새로고침 타이머 설정
-    const interval = setInterval(() => {
-      fetchNotes();
-    }, 10000); // 10초 = 10000밀리초
-
-    // 컴포넌트가 언마운트될 때 타이머를 정리합니다.
-    return () => clearInterval(interval);
   }, []);
 
+  // 브라우저가 렌더링(접속 또는 새로고침)될때마다 실행
   const fetchNotes = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/notes`)
-      .then((response) => response.json())
-      .then((data) => setNotes(data));
+    console.log("[fetchNotes] fetch notes from database");
+    fetch(`${REACT_APP_API_URL}/notes`)
+      .then((res) => res.json())
+      .then((notes) => {
+        setNotes(notes);
+      });
   };
 
-  const addNote = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/notes`, {
+  const addNote = (e) => {
+    e.preventDefault();
+    if (!newNote) return;
+
+    fetch(`${REACT_APP_API_URL}/notes`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content: newNote }),
-    }).then(() => {
-      fetchNotes();
-      setNewNote("");
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setNotes([...notes, data.note]);
+        setNewNote("");
+      });
   };
 
   const deleteNote = (id) => {
-    fetch(`${process.env.REACT_APP_API_URL}/notes/${id}`, {
+    console.log("[deleteNote] delete ONE note");
+    fetch(`${REACT_APP_API_URL}/notes/${id}`, {
       method: "DELETE",
-    }).then(() => fetchNotes());
+    }).then(() => {
+      setNotes(notes.filter((note) => note.id !== id));
+    });
   };
 
   const deleteNotes = () => {
-    fetch(`${process.env.REACT_APP_API_URL}/notes`, { method: "DELETE" }).then(
-      () => fetchNotes()
-    );
-  };
-
-  const requestAIAdvice = (userNote) => {
-    fetch(`${process.env.REACT_APP_API_URL}/ainotes`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ content: userNote }),
-    })
-      .then(() => {
-        fetchNotes();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+    console.log("[deleteNotes] delete ALL notes");
+    setNotes([]);
   };
 
   return (
     <div className="App">
-      <h1>학습 기록 애플리케이션 v1.0.1</h1>
+      <h1>학습 기록 애플리케이션</h1>
       <h3>오늘 학습한 내용을 기록해보세요.</h3>
       <textarea
         value={newNote}
@@ -78,23 +70,9 @@ function App() {
       <div>
         {notes.map((note) => (
           <div key={note.id} className="note">
-            <div className="note-content">
+            <button onClick={() => deleteNote(note.id)}>삭제</button>
+            <div>
               <strong>사용자 메모:</strong> {note.user_note}
-            </div>
-            {note.ai_note ? (
-              <div className="ai-note">
-                <strong>AI 추천 학습 내용:</strong> {note.ai_note}
-              </div>
-            ) : null}
-            <div className="note-actions">
-              {!note.ai_note && (
-                <button
-                  onClick={() => requestAIAdvice(note.user_note, note.id)}
-                >
-                  AI 조언 요청
-                </button>
-              )}
-              <button onClick={() => deleteNote(note.id)}>삭제</button>
             </div>
           </div>
         ))}
